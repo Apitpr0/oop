0e30b9f7d2e24cbf763b8c52cd5309f8a6175941
Apitpr0 <apitpro123@gmail.com>
2022-04-12 10:17:26 +0800

    change for loop to i<5 following array

--- Assignment/Loose Java Exercise/sumofarray.java
+++ Assignment/Loose Java Exercise/sumofarray.java
@@ -2,7 +2,7 @@ public class sumofarray {
     public static void main(String[] args) {
         int[] checkedArray = { 5, 2, 7, 9, 6 };
         int sum = 0;
-        for (int i = 0; i < checkedArray.length; i++) {
+        for (int i = 0; i < 5; i++) {
             sum = sum + checkedArray[i];
         }
         System.out.println("The sum of the array is " + sum);
