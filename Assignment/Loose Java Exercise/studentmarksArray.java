1f85d177875626a0ec0efe4ea04eac2f2d1a1f17
Apitpr0 <apitpro123@gmail.com>
2022-04-12 08:58:53 +0800

    learn array

---
+++ Assignment/Loose Java Exercise/studentmarksArray.java
@@ -0,0 +1,10 @@
+public class studentmarksArray {
+    public static void main(String[] args) {
+        int marks[] = { 95, 85, 75, 80, 65 };
+        System.out.print("\n marks[0]:" + marks[0]);
+        System.out.print("\n marks[1]:" + marks[1]);
+        System.out.print("\n marks[2]:" + marks[2]);
+        System.out.print("\n marks[3]:" + marks[3]);
+        System.out.print("\n marks[4]:" + marks[4]);
+    }
+}
