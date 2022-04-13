8d52cf440d2341c4296873dc5c37690b7cfae5cb
Apitpr0 <apitpro123@gmail.com>
2022-04-13 14:40:28 +0800

    two dimensional array test

---
+++ Assignment/Loose Java Exercise/twodim_array_students_marks.java
@@ -0,0 +1,15 @@
+class twodim_array_students_marks {
+    public static void main(String[] args) {
+        int marks_table[][] = {
+                { 83, 99, 74 },
+                { 88, 90, 72 },
+                { 89, 88, 82 },
+                { 98, 93, 75 },
+                { 78, 60, 65 }
+        };
+        System.out.println(marks_table[1][2]);
+        System.out.println(marks_table[3][0]);
+        System.out.println(marks_table[2][1]);
+        System.out.println(marks_table[4][1]);
+    }
+}
