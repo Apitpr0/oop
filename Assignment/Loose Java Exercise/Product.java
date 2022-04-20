b429a519b9d490af9a76c118a5fa30bb176e5ea5
Apitpr0 <apitpro123@gmail.com>
2022-04-20 15:54:39 +0800

    lab activity 5 and string method

---
+++ Assignment/Loose Java Exercise/Product.java
@@ -0,0 +1,21 @@
+class Product {
+    public static class prod {
+        int multiply(int No1, int No2) {
+            return (No1 * No2);
+        }
+
+        double multiply(double No1, double No2) {
+            return (No1 * No2);
+        }
+    }
+
+    public static void main(String args[]) {
+        int result1;
+        double result2;
+        prod obj = new prod();
+        result1 = obj.multiply(12, 13);
+        result2 = obj.multiply(2.5, 3.2);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
